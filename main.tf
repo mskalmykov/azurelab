4c292591dd0c3c43f8201cf15be1e370dba9983d
Mikhail Kalmykov <mikhail_kalmykov@epam.com>
2021-12-01 13:00:38 +0000

    AKS and ACR created. VM commented out

--- main.tf
+++ main.tf
@@ -33,72 +33,95 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create a virtual network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "main_vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = var.azure_region
-  resource_group_name = azurerm_resource_group.rg.name
-
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
-
-}
+#resource "azurerm_virtual_network" "vnet" {
+#  name                = "main_vnet"
+#  address_space       = ["10.0.0.0/16"]
+#  location            = var.azure_region
+#  resource_group_name = azurerm_resource_group.rg.name
+#
+#  subnet {
+#    name           = "subnet1"
+#    address_prefix = "10.0.1.0/24"
+#  }
+#
+#  subnet {
+#    name           = "subnet2"
+#    address_prefix = "10.0.2.0/24"
+#  }
+#
+#}
 
 # Public IP for VM
-resource "azurerm_public_ip" "vm1_public_ip" {
-  name                = "vm1_public_ip"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Dynamic"
-}
+#resource "azurerm_public_ip" "vm1_public_ip" {
+#  name                = "vm1_public_ip"
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#  allocation_method   = "Dynamic"
+#}
 
 # Network interface for VM
-resource "azurerm_network_interface" "vm1_nic" {
-  name                = "vm1_nic"
+#resource "azurerm_network_interface" "vm1_nic" {
+#  name                = "vm1_nic"
+#  location            = azurerm_resource_group.rg.location
+#  resource_group_name = azurerm_resource_group.rg.name
+#
+#  ip_configuration {
+#    name                          = "internal"
+#    subnet_id                     = azurerm_virtual_network.vnet.subnet.*.id[0]
+#    private_ip_address_allocation = "Dynamic"
+#    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+#  }
+#}
+
+#resource "azurerm_linux_virtual_machine" "admhost" {
+#  name                = "admhost"
+#  resource_group_name = azurerm_resource_group.rg.name
+#  location            = azurerm_resource_group.rg.location
+#  size                = "Standard_B1s"
+#  admin_username      = "azureuser"
+#  network_interface_ids = [
+#    azurerm_network_interface.vm1_nic.id,
+#  ]
+#
+#  admin_ssh_key {
+#    username   = "azureuser"
+#    public_key = file("~/.ssh/azure_key.pub")
+#  }
+#
+#  os_disk {
+#    caching              = "ReadWrite"
+#    storage_account_type = "Standard_LRS"
+#  }
+#
+#  source_image_reference {
+#    publisher = "Debian"
+#    offer     = "debian-11"
+#    sku       = "11-gen2"
+#    version   = "latest"
+#  }
+#}
+
+resource "azurerm_kubernetes_cluster" "aks1" {
+  name                = "aks1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aks1"
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_virtual_network.vnet.subnet.*.id[0]
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
 }
 
-resource "azurerm_linux_virtual_machine" "admhost" {
-  name                = "admhost"
+resource "azurerm_container_registry" "acr" {
+  name                = "mskepamdiplomaacr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.vm1_nic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/azure_key.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Debian"
-    offer     = "debian-11"
-    sku       = "11-gen2"
-    version   = "latest"
-  }
+  sku                 = "Basic"
 }
-
-
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "resource_group_id" {
-  value = azurerm_resource_group.rg.id
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.aks1.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.aks1.kube_config_raw
+
+  sensitive = true
 }
