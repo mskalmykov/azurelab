283d46a23a03584ae25fecc630cbf9fa0d239fb1
Mikhail Kalmykov <Mikhail_Kalmykov@epam.com>
2021-12-05 21:04:29 +0000

    Minor changes

--- main.tf
+++ main.tf
@@ -27,92 +27,36 @@ resource "azurerm_resource_group" "rg" {
   location = var.azure_region
 
   tags = {
-    Environment = "Test"
-    Owner       = "mskawslearn1@gmail.com"
+    Owner = "mskawslearn1@gmail.com"
   }
 }
 
-# Create a virtual network
-#resource "azurerm_virtual_network" "vnet" {
-#  name                = "main_vnet"
-#  address_space       = ["10.0.0.0/16"]
-#  location            = var.azure_region
-#  resource_group_name = azurerm_resource_group.rg.name
-#
-#  subnet {
-#    name           = "subnet1"
-#    address_prefix = "10.0.1.0/24"
-#  }
-#
-#  subnet {
-#    name           = "subnet2"
-#    address_prefix = "10.0.2.0/24"
-#  }
-#
-#}
-
-# Public IP for VM
-#resource "azurerm_public_ip" "vm1_public_ip" {
-#  name                = "vm1_public_ip"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  location            = azurerm_resource_group.rg.location
-#  allocation_method   = "Dynamic"
-#}
-
-# Network interface for VM
-#resource "azurerm_network_interface" "vm1_nic" {
-#  name                = "vm1_nic"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#
-#  ip_configuration {
-#    name                          = "internal"
-#    subnet_id                     = azurerm_virtual_network.vnet.subnet.*.id[0]
-#    private_ip_address_allocation = "Dynamic"
-#    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
-#  }
-#}
-
-#resource "azurerm_linux_virtual_machine" "admhost" {
-#  name                = "admhost"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  location            = azurerm_resource_group.rg.location
-#  size                = "Standard_B1s"
-#  admin_username      = "azureuser"
-#  network_interface_ids = [
-#    azurerm_network_interface.vm1_nic.id,
-#  ]
-#
-#  admin_ssh_key {
-#    username   = "azureuser"
-#    public_key = file("~/.ssh/azure_key.pub")
-#  }
-#
-#  os_disk {
-#    caching              = "ReadWrite"
-#    storage_account_type = "Standard_LRS"
-#  }
-#
-#  source_image_reference {
-#    publisher = "Debian"
-#    offer     = "debian-11"
-#    sku       = "11-gen2"
-#    version   = "latest"
-#  }
-#}
-
-resource "azurerm_kubernetes_cluster" "aks1" {
-  name                = "aks1"
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.cluster_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aks1"
+  dns_prefix          = var.dns_prefix
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
 
   default_node_pool {
-    name                = "default"
-    min_count           = 1
-    max_count           = 2
-    vm_size             = "Standard_B2s"
+    name      = "default"
+    min_count = 1
+    max_count = 2
+    vm_size   = "Standard_B2s"
+    #vm_size             = "Standard_DS2_v2"
     enable_auto_scaling = true
+  }
+
+  auto_scaler_profile {
+    scale_down_delay_after_add = "2m"
+    scale_down_unneeded        = "2m"
   }
 
   identity {
@@ -132,7 +76,7 @@ resource "azurerm_container_registry" "acr" {
 resource "azurerm_role_assignment" "aks_to_acr_role" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks1.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.k8s.kubelet_identity[0].object_id
 }
 
 resource "azurerm_mariadb_server" "dbsrv" {
--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "azure_region" {
   default = "westeurope"
 }
 
+variable "location" {
+  default = "West Europe"
+}
+
 variable "resource_group_name" {
   default = "EPAM_Diploma"
 }
@@ -10,3 +14,16 @@ variable "DB_PASSWORD" {
   description = "Password for MariaDB admin (get from environment)"
   type        = string
 }
+
+variable "ssh_public_key" {
+  default = "~/.ssh/azure_key.pub"
+}
+
+variable "dns_prefix" {
+  default = "aks1"
+}
+
+variable "cluster_name" {
+  default = "aks1"
+}
+
